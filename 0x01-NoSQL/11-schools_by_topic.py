e6e49a20976673c75354985e340dd5d7c66e5a19
kevinnevs <kevinnjuguna2@gmail.com>
2023-10-17 04:12:19 -0700

    fixing indentation

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -10,7 +10,7 @@ def schools_by_topic(mongo_collection, topic):
         'topics': {
             '$elemMatch': {
                 '$eq': topic,
-                },
             },
         },
+    },
     return [doc for doc in mongo_collection.find(topic_filter)]
