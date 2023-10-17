75adaac5edc6f8e8dcdde101d1b697a400b87f52
kevinnevs <kevinnjuguna2@gmail.com>
2023-10-17 04:13:48 -0700

    removing comma

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -12,5 +12,5 @@ def schools_by_topic(mongo_collection, topic):
                 '$eq': topic,
             },
         },
-    },
+    }
     return [doc for doc in mongo_collection.find(topic_filter)]
